1704a31a611907b9217374de97ec2222c378fce0
Prasad Sivanandan <chazeprasad@gmail.com>
2020-10-12 17:57:14 +0530

    arranges listing order

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,11 @@ import Container from '../src/component/core/Container'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      order: ['Landing', 'Payment', 'Table', 'Pagination'],
+    },
+  },
 }
 
 addDecorator(story => <Container>{story()}</Container>)
